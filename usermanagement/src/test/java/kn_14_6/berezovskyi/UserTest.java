a23d553b99ac95eff40423e54b555a74fec43842
heorhiiberezovskyi <heorhii.berezovskyi@nure.ua>
2016-11-18 18:57:09 +0200

    Workspace added

---
+++ usermanagement/src/test/java/kn_14_6/berezovskyi/UserTest.java
@@ -0,0 +1,35 @@
+package kn_14_6.berezovskyi;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import junit.framework.TestCase;
+
+public class UserTest extends TestCase {
+	
+	private User user;
+	private Date dateOfBirthd;
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		user = new User();
+		
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(1984, Calendar.MAY, 26);
+		dateOfBirthd = calendar.getTime();
+		
+	}
+	
+	
+	public void testGetFullName() {
+		user.setFirstName("John");
+		user.setLastName("Doe");
+		assertEquals("Doe, John", user.getFullName());
+	}
+	
+	public void testGetAge() {
+		user.setDateOfBirthd(dateOfBirthd);
+		assertEquals(2016 - 1984, user.getAge());
+	}
+
+}
